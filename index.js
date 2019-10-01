9ed988c4dfd67438a66e6ffe8c17ce9a06afa55a
Ramon Chavez <rachavez1974@gmail.com>
2019-10-01 15:10:37 -0400

    Finish js-principles-scope-lab-online-web-sp-000

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "Trump";
+
+function upperCaseCustomerName(){
+  customerName = "BOB";
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "Steve Miller";
+}
